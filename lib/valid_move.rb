0a203148702221c27c4d517e8d26bde478598d1d
Xavier Carty <xaviercarty@gmail.com>
2019-11-20 22:54:49 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,11 +1,11 @@
 # code your #valid_move? method here
 def valid_move?(board , index)
   if position_taken?(board ,index)
-    return false 
+    return false
   else
     return true
   end
-endx
+end
 
 
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
